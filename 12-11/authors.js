2fb303ed478f2b701e060f9f1f16121c81566d17
Liam Frager <liam856@revature.net>
2024-12-11 16:32:34 -0500

    add async/await version

--- 12-11/authors.js
+++ 12-11/authors.js
@@ -2,26 +2,36 @@ function showAuthors() {
     fetch("https://jsonplaceholder.typicode.com/users")
         .then(res => {
             res.json().then(data => {
-                document.getElementById("button").remove();
-                const list = document.createElement("ul");
-                list.id = "list";
-                data.map(author => {
-                    const newAuthor = document.createElement("li");
-                    const authorName = document.createElement("strong");
-                    authorName.innerHTML = author.name + ", ";
-                    newAuthor.append(authorName);
-                    const emailAnchor = document.createElement("a")
-                    emailAnchor.href = `mailto:${author.email}`;
-                    const authorEmail = document.createElement("i");
-                    authorEmail.innerHTML = author.email;
-                    emailAnchor.append(authorEmail);
-                    newAuthor.append(emailAnchor);
-                    list.append(newAuthor);
-                })
-                document.getElementsByTagName("body")[0].append(list);
+                makeList(data);
             });
         })
-        .catch(() => {
-
+        .catch(err => {
+            console.error(err);
         });
+}
+
+async function showAuthorsAsync() {
+    res = await fetch("https://jsonplaceholder.typicode.com/users")
+    data = await res.json()
+    makeList(data);
+}
+
+function makeList(data) {
+    document.getElementById("button").remove();
+    const list = document.createElement("ul");
+    list.id = "list";
+    data.map(author => {
+        const newAuthor = document.createElement("li");
+        const authorName = document.createElement("strong");
+        authorName.innerHTML = author.name + ", ";
+        newAuthor.append(authorName);
+        const emailAnchor = document.createElement("a")
+        emailAnchor.href = `mailto:${author.email}`;
+        const authorEmail = document.createElement("i");
+        authorEmail.innerHTML = author.email;
+        emailAnchor.append(authorEmail);
+        newAuthor.append(emailAnchor);
+        list.append(newAuthor);
+    })
+    document.getElementsByTagName("body")[0].append(list);
 }
